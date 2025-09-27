82891791115e541aa40724ef09065e04cbe7d1a2
Nutchanon Kullasit <nutchanonkulsit@gmail.com>
2025-09-28 01:31:33 +0700

    create components angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ManageUserComponent } from './pages/admin/manage-user/manage-user.component';
+import { KpiComponent } from './pages/user/kpi/kpi.component';
+import { LayoutComponent } from './layouts/layout/layout.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ManageUserComponent,
+    KpiComponent,
+    LayoutComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/pages/user/kpi/kpi.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kpi',
+  standalone: false,
+  templateUrl: './kpi.component.html',
+  styleUrl: './kpi.component.css'
+})
+export class KpiComponent {
+
+}
