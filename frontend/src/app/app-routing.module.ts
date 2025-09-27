0c5d0938b45b3ee751c66784794cdbfb802ca81c
Nutchanon Kullasit <nutchanonkulsit@gmail.com>
2025-09-28 04:16:11 +0700

    routing augualr

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { KpiComponent } from './pages/user/kpi/kpi.component';
+import { LayoutComponent } from './layouts/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  {
+    path: 'user',
+    component: LayoutComponent,
+    children: [{ path: 'kpi', component: KpiComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
